1ddd1dc27977da217c892b64b2a665ac81ba1b9d
Lalitha <lalitha.d339@gmail.com>
2023-09-05 05:32:57 +0530

    terraform variables

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 yum install ansible -y &>>/opt/userdata.log
-ansible-playbook -i localhost, -U https://github.com/LalithaDendukuri/roboshop-ansible.git main.yml -e component=${component} &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/LalithaDendukuri/roboshop-ansible.git main.yml -e component=${component} &>>/opt/userdata.log
