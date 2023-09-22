fb8d95598147b961b100268909574a7be44de2c3
Lalitha <lalitha.d339@gmail.com>
2023-09-22 06:54:02 +0530

    terraform variables

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_instance" "instance" {
 
 resource "aws_route53_record" "www" {
   zone_id = var.zone_id
-  name    = "route53-${var.env}"
+  name    = "rabbitmq-${var.env}"
   type    = "A"
   ttl     = 300
   records = [aws_instance.instance.private_ip]
